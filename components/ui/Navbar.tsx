2427d27f5177bad51ff1a5f0950b0b401522ca44
Aniket Bindhani <aniketbindhani44@gmail.com>
2024-01-07 22:25:44 +0530

    Fixed dark mode v1

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
 
     return (
         <header className="sticky z-50 top-0 my-10">
-            <nav className="bg-white dark:bg-black border-gray-200 px-4 lg:px-6 py-2.5">
+            <nav className="bg-white dark:bg-[#1C1E21] border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-center items-center mx-auto sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-lg">
                     <Link className='text-3xl font-bold' href="/">Aniket Bindhani <DarkModeToggle /></Link>
                 </div>
